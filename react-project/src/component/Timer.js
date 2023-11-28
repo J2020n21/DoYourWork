755e8ce4d1932d4084a1c9d8930e02cea16e5159
Son Jiui <jiui0103@naver.com>
2023-11-28 19:42:51 +0900

    processing; subtle change

--- react-project/src/component/Timer.js
+++ react-project/src/component/Timer.js
@@ -4,29 +4,45 @@ export default function Timer() {
   const [min, setMin] = useState(0);
   const [sec, setSec] = useState(0);
   const [timerGo, setTimerGo] = useState(false);
-//00:00 
-  let timerId;
-  let second;
+//1.초>분 넘어가게 처리 2.버튼 동작하기
+  let timerSecId;
+  let timerMinId;
 
   useEffect(()=>{
-    if(timerId){clearInterval(timerId)}
-    timerId = setInterval(() => {
-      setSec(s => s+1)
-    }, 1000);
-    if(sec%60 == 0){setSec(0); setMin(m=>m+1)}
+    if(timerSecId){clearInterval(timerSecId)}
+    if(timerMinId){clearInterval(timerMinId)}
+    if(timerGo){
+      timerSecId = setInterval(() => {
+        setSec(s => s+1)
+      }, 1000);
+      timerMinId = setInterval(()=>{
+        setMin(m => m+1)
+      },60000)
+    }
   },[timerGo])
 
+  const startTimer = () =>{
+    setTimerGo(true)
+  }
+
+  const stopTimer = () =>{ //작동 정지.
+    setTimerGo(false)
+  }
+
+  const resetTimer = () =>{
+    setTimerGo(false)
+  }
 
   return (
     //setInterval, setTimeout
-    //기능 1.뽀모도로 타이머(25/5)작동. 세션 기억할 필요가 있음. db활용
+    //기능 1.뽀모도로 타이머(25/5)작동. 세션 기억 db활용
     //기능 2.일반 타이머. 0초부터 시간이 stop 누를때까지 늘어남.
   <div style={{'border': '1px solid black'}}>
     
     <div>Timer</div>
-    <button type='button' >시작</button>
-    <button type='button'>멈춤</button>
-    <button type='button' >초기화</button>
+    <button type='button' onClick={startTimer}>시작</button>
+    <button type='button' onClick={stopTimer}>멈춤</button>
+    <button type='button' onClick={resetTimer}>초기화</button>
     <p>{min}:{sec}</p>
   </div>
   )
