93e04d29e5a6cbe3bbee61b3bb78edd83e24446b
Jiui <jiui0103@naver.com>
2023-12-04 23:15:27 +0900

    make basic timer function

--- react-project/src/component/Timer.js
+++ react-project/src/component/Timer.js
@@ -3,31 +3,49 @@ import React, {useState, useEffect} from 'react'
 export default function Timer() {
   const [min, setMin] = useState(0);
   const [sec, setSec] = useState(0);
-  const [timerGo, setTimerGo] = useState(false);
+  const [go, setGo] = useState(true);
 //00:00 
-  let timerId;
-  let second;
+  var timerId;
 
   useEffect(()=>{
-    if(timerId){clearInterval(timerId)}
+    if(go){
     timerId = setInterval(() => {
-      setSec(s => s+1)
-    }, 1000);
-    if(sec%60 == 0){setSec(0); setMin(m=>m+1)}
-  },[timerGo])
+      setSec(sec+1)
 
+      if(sec === 59){
+        setMin(min+1)
+        setSec(0)
+      }
+    }, 1000);
+  }
+    return () =>{clearInterval(timerId)}
+
+  })
+
+  const timerReset = () =>{
+    setSec(0)
+    setMin(0)
+    clearInterval(timerId)
+  }
+
+  const timerStop = () =>{
+    setGo(false)
+  }
+
+  const timerGo = () =>{
+    setGo(true)
+  }
 
   return (
     //setInterval, setTimeout
     //기능 1.뽀모도로 타이머(25/5)작동. 세션 기억할 필요가 있음. db활용
-    //기능 2.일반 타이머. 0초부터 시간이 stop 누를때까지 늘어남.
   <div style={{'border': '1px solid black'}}>
     
     <div>Timer</div>
-    <button type='button' >시작</button>
-    <button type='button'>멈춤</button>
-    <button type='button' >초기화</button>
-    <p>{min}:{sec}</p>
+    <button type='button' onClick={timerReset}>초기화</button>
+    <button type='button' onClick={timerStop}>멈춤</button>
+    <button type='button' onClick={timerGo}>재시작</button>
+    <p>{min<10? '0'+min:min}:{sec<10? '0'+sec:sec}</p>
   </div>
   )
 }
